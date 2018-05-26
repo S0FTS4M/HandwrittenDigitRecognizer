404996e31e31caa0231ab69737d64b84988dd9ba
Melih <mlhtncc@gmail.com>
2018-05-27 01:31:57 +0300

    Basic Drawing Stuff
    -Not done yet.

---
+++ HandwrittenDigitRecognizer/Assets/Scripts/DrawLine2D.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine2D : MonoBehaviour
+{
+    private Camera mainCamera;
+    private LineRenderer lineRenderer;
+	private List<Vector2> points;
+
+	public LineRenderer LineRenderer
+	{
+		get
+		{
+			return lineRenderer;
+		}
+	}
+
+	public List<Vector2> Points
+	{
+		get
+		{
+			return points;
+		}
+	}
+
+	private void Awake ()
+	{
+		if ( lineRenderer == null )
+		{
+			Debug.LogWarning ( "DrawLine: Line Renderer not assigned, Adding and Using default Line Renderer." );
+			CreateDefaultLineRenderer ();
+		}
+
+		if ( mainCamera == null ) {
+			mainCamera = Camera.main;
+		}
+
+		points = new List<Vector2> ();
+	}
+
+	private void Update ()
+	{
+        if (Input.GetMouseButtonDown(0))
+        {
+            Reset();
+        }
+
+        if ( Input.GetMouseButton ( 0 ) )
+		{
+			Vector2 mousePosition = mainCamera.ScreenToWorldPoint ( Input.mousePosition );
+			Vector2 lineRendererPosition = lineRenderer.transform.position;
+
+			if ( !points.Contains ( mousePosition ) && mousePosition.y > ProgramController.programController.drawBoundY + lineRenderer.startWidth / 2 )
+			{
+				points.Add ( mousePosition );
+				lineRenderer.positionCount = points.Count;
+				lineRenderer.SetPosition ( lineRenderer.positionCount - 1, mousePosition);
+			}
+		}
+
+	}
+
+	public void Reset ()
+	{
+		if ( lineRenderer != null )
+		{
+			lineRenderer.positionCount = 0;
+		}
+		if ( points != null )
+		{
+			points.Clear ();
+		}
+	}
+
+	private void CreateDefaultLineRenderer ()
+	{
+		lineRenderer = gameObject.AddComponent<LineRenderer>();
+		lineRenderer.positionCount = 0;
+        lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+		lineRenderer.startColor = Color.white;
+		lineRenderer.endColor = Color.white;
+		lineRenderer.startWidth = 0.2f;
+		lineRenderer.endWidth = 0.2f;
+		lineRenderer.useWorldSpace = true;
+	}
+}
---
+++ HandwrittenDigitRecognizer/Assets/Scripts/ProgramController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgramController : MonoBehaviour
+{
+    public static ProgramController programController;
+
+    Camera mainCamera;
+    bool grabImage;
+
+    float width;
+    float height;
+    public float drawBoundY;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (programController != null)
+            return;
+
+        programController = this;
+        mainCamera = Camera.main;
+
+        height = mainCamera.orthographicSize * 2;
+        width = mainCamera.aspect * height;
+        drawBoundY = (int) mainCamera.transform.position.y + (height / 2) - width;
+
+        DrawBound();
+    }
+
+    private void DrawBound()
+    {
+        GameObject go = new GameObject("Line");
+        LineRenderer lineRenderer = go.AddComponent<LineRenderer>();
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(0, new Vector3(-width / 2, drawBoundY - .05f, 0));
+        lineRenderer.SetPosition(1, new Vector3(width / 2, drawBoundY - .05f, 0));
+        lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+        lineRenderer.startColor = Color.white;
+        lineRenderer.endColor = Color.white;
+        lineRenderer.startWidth = 0.01f;
+        lineRenderer.endWidth = 0.01f;
+        lineRenderer.useWorldSpace = true;
+    }
+
+    private void Update()
+    {
+        //Press space to start the screen grab
+        if (Input.GetKeyDown(KeyCode.Space))
+            grabImage = true;
+    }
+
+    private void OnPostRender()
+    {
+        if (grabImage)
+        {
+            //Create a new texture with the width and height of the screen
+            Texture2D texture = new Texture2D(Screen.width, Screen.width, TextureFormat.RGB24, false);
+            
+            //Read the pixels in the Rect starting at 0,0 and ending at the screen's width and height
+            texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.width), 0, 0, false);
+            texture.Apply();
+
+            //Reset the grab state
+            grabImage = false;
+
+            //TextureScale.Bilinear(texture, 28, 28);
+
+            byte[] bytes = texture.EncodeToPNG();
+            System.IO.File.WriteAllBytes(@"D:\workspace\unity\image.png", bytes);
+        }
+    }
+}
